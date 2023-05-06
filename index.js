13f09ea84de1894d06beccf6a756a4e6382a571a
armeen <armeenfarange@yahoo.com>
2023-05-06 16:18:23 -0700

    Change Layout and Complete Functionality

--- index.js
+++ index.js
@@ -1,71 +1,132 @@
-function randomizeGoalkeepers() {
-    const goalkeepers = [];
+function fillNames() {
+    // Define the player names for each position
+    const goalkeepers = ["GK 1", "GK 2"];
+    const defenders = ["Defender 1", "Defender 2", "Defender 3", "Defender 4", "Defender 5", "Defender 6", "Defender 7", "Defender 8"];
+    const midfielders = ["Midfielder 1", "Midfielder 2", "Midfielder 3", "Midfielder 4", "Midfielder 5", "Midfielder 6"];
+    const forwards = ["Forward 1", "Forward 2", "Forward 3", "Forward 4", "Forward 5", "Forward 6"];
 
-    // Get all input fields and add their values to the goalkeepers array
-    for (let i = 1; i <= 4; i++) {
-        const goalkeeperInput = document.getElementById(`goalkeeper${i}`);
-        const name = goalkeeperInput.value.trim();
-        if (name !== '') {
-            goalkeepers.push(name);
-        }
-    }
+    // Fill in the player names for each position
+    document.getElementsByName("gk1")[0].value = goalkeepers[Math.floor(Math.random() * goalkeepers.length)];
+    document.getElementsByName("gk2")[0].value = goalkeepers[Math.floor(Math.random() * goalkeepers.length)];
+    document.getElementsByName("def1")[0].value = defenders[Math.floor(Math.random() * defenders.length)];
+    document.getElementsByName("def2")[0].value = defenders[Math.floor(Math.random() * defenders.length)];
+    document.getElementsByName("def3")[0].value = defenders[Math.floor(Math.random() * defenders.length)];
+    document.getElementsByName("def4")[0].value = defenders[Math.floor(Math.random() * defenders.length)];
+    document.getElementsByName("def5")[0].value = defenders[Math.floor(Math.random() * defenders.length)];
+    document.getElementsByName("def6")[0].value = defenders[Math.floor(Math.random() * defenders.length)];
+    document.getElementsByName("def7")[0].value = defenders[Math.floor(Math.random() * defenders.length)];
+    document.getElementsByName("def8")[0].value = defenders[Math.floor(Math.random() * defenders.length)];
+    document.getElementsByName("mid1")[0].value = midfielders[Math.floor(Math.random() * midfielders.length)];
+    document.getElementsByName("mid2")[0].value = midfielders[Math.floor(Math.random() * midfielders.length)];
+    document.getElementsByName("mid3")[0].value = midfielders[Math.floor(Math.random() * midfielders.length)];
+    document.getElementsByName("mid4")[0].value = midfielders[Math.floor(Math.random() * midfielders.length)];
+    document.getElementsByName("mid5")[0].value = midfielders[Math.floor(Math.random() * midfielders.length)];
+    document.getElementsByName("mid6")[0].value = midfielders[Math.floor(Math.random() * midfielders.length)];
+    document.getElementsByName("fwd1")[0].value = forwards[Math.floor(Math.random() * forwards.length)];
+    document.getElementsByName("fwd2")[0].value = forwards[Math.floor(Math.random() * forwards.length)];
+    document.getElementsByName("fwd3")[0].value = forwards[Math.floor(Math.random() * forwards.length)];
+    document.getElementsByName("fwd4")[0].value = forwards[Math.floor(Math.random() * forwards.length)];
+    document.getElementsByName("fwd5")[0].value = forwards[Math.floor(Math.random() * forwards.length)];
+    document.getElementsByName("fwd6")[0].value = forwards[Math.floor(Math.random() * forwards.length)];
 
-    // Shuffle the names array
-    for (let i = goalkeepers.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [goalkeepers[i], goalkeepers[j]] = [goalkeepers[j], goalkeepers[i]];
-    }
-
-    // Clear any existing team lists
-    const team1GoalkeeperList = document.getElementById('team1Goalkeepers');
-    const team2GoalkeeperList = document.getElementById('team2Goalkeepers');
-    team1GoalkeeperList.innerHTML = '';
-    team2GoalkeeperList.innerHTML = '';
-
-    // Add the first 2 names to team 1 and the rest to team 2
-    for (let i = 0; i < 4; i++) {
-        const nameLi = document.createElement('li');
-        nameLi.innerText = goalkeepers[i];
-        if (i < 2) {
-            team1GoalkeeperList.appendChild(nameLi);
-        } else {
-            team2GoalkeeperList.appendChild(nameLi);
-        }
-    }
+    // Prevent the form from submitting
+    event.preventDefault();
 }
 
-function randomizeMidfielders() {
-    const midfielders = [];
+function randomize() {
+    const goalkeepers = [
+        document.getElementsByName("gk1")[0].value,
+        document.getElementsByName("gk2")[0].value,
+    ];
+    const defenders = [
+        document.getElementsByName("def1")[0].value,
+        document.getElementsByName("def2")[0].value,
+        document.getElementsByName("def3")[0].value,
+        document.getElementsByName("def4")[0].value,
+        document.getElementsByName("def5")[0].value,
+        document.getElementsByName("def6")[0].value,
+        document.getElementsByName("def7")[0].value,
+        document.getElementsByName("def8")[0].value,
+    ];
+    const midfielders = [
+        document.getElementsByName("mid1")[0].value,
+        document.getElementsByName("mid2")[0].value,
+        document.getElementsByName("mid3")[0].value,
+        document.getElementsByName("mid4")[0].value,
+        document.getElementsByName("mid5")[0].value,
+        document.getElementsByName("mid6")[0].value,
+    ];
+    const forwards = [
+        document.getElementsByName("fwd1")[0].value,
+        document.getElementsByName("fwd2")[0].value,
+        document.getElementsByName("fwd3")[0].value,
+        document.getElementsByName("fwd4")[0].value,
+        document.getElementsByName("fwd5")[0].value,
+        document.getElementsByName("fwd6")[0].value,
+    ];
 
-    // Get all input fields and add their values to the midfielders array
-    for (let i = 1; i <= 4; i++) {
-        const midfielderInput = document.getElementById(`midfielder${i}`);
-        const name = midfielderInput.value.trim();
-        if (name !== '') {
-            midfielders.push(name);
-        }
-    }
+    // Shuffle the players in each position
+    const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
+    const shuffledGoalkeepers = shuffleArray(goalkeepers);
+    const shuffledDefenders = shuffleArray(defenders);
+    const shuffledMidfielders = shuffleArray(midfielders);
+    const shuffledForwards = shuffleArray(forwards);
 
-    // Shuffle the names array
-    for (let i = midfielders.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [midfielders[i], midfielders[j]] = [midfielders[j], midfielders[i]];
-    }
+    // Divide players into two teams
+    const team1 = [
+        shuffledGoalkeepers[0],
+        shuffledDefenders[0],
+        shuffledDefenders[1],
+        shuffledDefenders[2],
+        shuffledDefenders[3],
+        shuffledMidfielders[0],
+        shuffledMidfielders[1],
+        shuffledMidfielders[2],
+        shuffledForwards[0],
+        shuffledForwards[1],
+        shuffledForwards[2],
+    ];
+    const team2 = [
+        shuffledGoalkeepers[1],
+        shuffledDefenders[4],
+        shuffledDefenders[5],
+        shuffledDefenders[6],
+        shuffledDefenders[7],
+        shuffledMidfielders[3],
+        shuffledMidfielders[4],
+        shuffledMidfielders[5],
+        shuffledForwards[3],
+        shuffledForwards[4],
+        shuffledForwards[5],
+    ];
 
-    // Clear any existing team lists
-    const team1MidfielderList = document.getElementById('team1Midfielders');
-    const team2MidfielderList = document.getElementById('team2Midfielders');
-    team1MidfielderList.innerHTML = '';
-    team2MidfielderList.innerHTML = '';
+    // Display the team members in two lists
+    // Two empty unordered lists (team1List,team2List)
+    const team1List = document.createElement("ul");
+    const team2List = document.createElement("ul");
 
-    // Add the first 2 names to team 1 and the rest to team 2
-    for (let i = 0; i < 4; i++) {
-        const nameLi = document.createElement('li');
-        nameLi.innerText = midfielders[i];
-        if (i < 2) {
-            team1MidfielderList.appendChild(nameLi);
-        } else {
-            team2MidfielderList.appendChild(nameLi);
-        }
-    }
+    // Iterate over each player in team1 and team2 using the forEach() method.
+    //For each player, it creates a new list item (<li>) using document.createElement("li"),
+    team1.forEach((player) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = player;
+        team1List.appendChild(listItem);
+    });
+
+    team2.forEach((player) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = player;
+        team2List.appendChild(listItem);
+    });
+
+    const teamListsContainer = document.createElement("div");
+    teamListsContainer.classList.add("team-lists");
+    teamListsContainer.appendChild(team1List);
+    teamListsContainer.appendChild(team2List);
+
+    const teamForm = document.querySelector(".team-form");
+    teamForm.appendChild(teamListsContainer);
+
+    // Prevent the form from submitting
+    event.preventDefault();
 }
